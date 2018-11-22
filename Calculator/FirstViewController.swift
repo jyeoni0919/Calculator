9a4756c47aa6d1edb06f821eec40441830b8987f
gongjiwon <rag9008@naver.com>
2018-11-22 16:22:28 +0900

    전체적인 코드 리뷰 반영 commit

--- Calculator/FirstViewController.swift
+++ Calculator/FirstViewController.swift
@@ -8,42 +8,75 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    //MARK: properties
-    let operators = ["+", "-", "*", "/"] //사칙 연산자
-    let cellIdentifier = "cell"
-    var selectedOperator = ""
+class FirstViewController: UIViewController {
+    //MARK: Properties
+    //private let operators = ["+", "-", "*", "/"] //사칙 연산자
+    private let cellIdentifier = "operatorCell"
+    
+    enum Operator: String, CaseIterable {
+        case add = "+"
+        case subtraction = "-"
+        case multiply = "*"
+        case division = "/"
+    }
 
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    //MARK: UITableViewDelegate
+}
+
+//MARK: UITableViewDelegate
+extension FirstViewController: UITableViewDelegate {
     //row가 select되었을 때 호출되는 메서드
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedOperator = operators[indexPath.row]
-        
-        guard let svc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") as? SecondViewController else {
-            return
+        var selectedOperator = ""
+        //selectedOperator = operators[indexPath.row]
+        switch indexPath.row {
+        case 0:
+            selectedOperator = Operator.add.rawValue
+        case 1:
+            selectedOperator = Operator.subtraction.rawValue
+        case 2:
+            selectedOperator = Operator.multiply.rawValue
+        case 3:
+            selectedOperator = Operator.division.rawValue
+        default:
+            print("잘못된 접근")
         }
+        
+        guard let svc = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else { return }
         
         //데이터 전달
         svc.navigationItem.title = selectedOperator
-
-        self.navigationController?.pushViewController(svc, animated: true)
+        
+        navigationController?.pushViewController(svc, animated: true)
     }
-    
-    //MARK: UITableViewDataSource
+}
+
+//MARK: UITableViewDataSource
+extension FirstViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return operators.count
+        //return operators.count
+        return Operator.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
-        cell.textLabel?.text = operators[indexPath.row]
-        
+        //cell.textLabel?.text = operators[indexPath.row]
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = Operator.add.rawValue
+        case 1:
+            cell.textLabel?.text = Operator.subtraction.rawValue
+        case 2:
+            cell.textLabel?.text = Operator.multiply.rawValue
+        case 3:
+            cell.textLabel?.text = Operator.division.rawValue
+        default:
+            cell.textLabel?.text = ""
+        }
         return cell
     }
 }
-
--- Calculator/SecondViewController.swift
+++ Calculator/SecondViewController.swift
@@ -11,24 +11,33 @@ import UIKit
 class SecondViewController: UIViewController {
     //MARK: IBOutlet
     @IBOutlet weak var opButton: UIButton! //연산자 버튼(+,-,*,/)
-    @IBOutlet weak var display: UILabel! //결과 보여주는 view 
+    @IBOutlet weak var display: UILabel! //결과 보여주는 view
+    @IBOutlet var digitButtons: [UIButton]! //숫자 버튼들
+    @IBOutlet var superviewOfDisplay: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        opButton.setTitle(self.navigationItem.title, for: .normal)
+        opButton.setTitle(navigationItem.title, for: .normal)
+        
+        opButton.layer.cornerRadius = 5.0
+        superviewOfDisplay.layer.cornerRadius = 5.0
+        for i in 0..<digitButtons.count {
+            digitButtons[i].layer.cornerRadius = 5.0
+        }
     }
     
     //display값은 String니깐 실제 계산을 위해서 Double로 바꿔주기
-    var displayValue: Double {
-        if let value = Double(display.text!) {
+    private var displayValue: Double {
+        guard let displayText = display.text else { return 0.0 }
+        if let value = Double(displayText) {
             return value
         }
         return 0
     }
     
     //사용자가 숫자를 입력중인지 아닌지 판단하기 위한 변수
-    var userIsInTheMiddleOfTyping = false
+    private var isUserInTheMiddleOfTyping = false
     
     //MARK: IBAction
     //숫자를 눌렀을 때
@@ -38,14 +47,15 @@ class SecondViewController: UIViewController {
         }
         
         //아직 숫자를 입력중이라면
-        if(userIsInTheMiddleOfTyping) {
-            let textCurrentlyInDisplay = display.text!
+        if(isUserInTheMiddleOfTyping) {
+            guard let displayText = display.text else { return }
+            let textCurrentlyInDisplay = displayText
             display.text = textCurrentlyInDisplay + digit
         } else {
             //입력중이 아니면
             display.text = digit
         }
-        userIsInTheMiddleOfTyping = true
+        isUserInTheMiddleOfTyping = true
     }
     
     private var sum: Double = 0.0
@@ -55,32 +65,32 @@ class SecondViewController: UIViewController {
         
         //mathmaticalSymbol => +,-,*,/
         if let mathmaticalSymbol = sender.currentTitle {
-            userIsInTheMiddleOfTyping = false
+            isUserInTheMiddleOfTyping = false
             
             switch mathmaticalSymbol {
             case "+":
                 //처음으로 숫자를 입력하는 거면
-                if(sum == 0) { sum = displayValue }
+                if sum == 0 { sum = displayValue }
                 //이미 누적된 값이 있다면
                 else { sum += displayValue }
                 break
             case "-":
-                if(sum == 0) { sum = displayValue }
+                if sum == 0 { sum = displayValue }
                 else { sum -= displayValue }
                 break
             case "*":
-                if(sum == 0) { sum = displayValue }
+                if sum == 0 { sum = displayValue }
                 else { sum *= displayValue }
                 break
             case "/":
-                if(sum == 0) { sum = displayValue }
+                if sum == 0 { sum = displayValue }
                 else { sum /= displayValue }
                 break
             default:
                 print("잘못된 operator입니다.")
                 return
             }
-            self.display.text = String(sum)
+            display.text = String(sum)
         }
     }
 }
